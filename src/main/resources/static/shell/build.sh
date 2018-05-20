f2ff1cf897ba101e5e9f33bb7228913ce59534ed
Rencp <kssion@yeah.net>
2018-05-20 23:50:19 +0800

    v 1.0.1

--- src/main/resources/static/shell/build.sh
+++ src/main/resources/static/shell/build.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 # $1 UUID
 # $2 仓库地址
-# $3 Git分支
-# $4 项目部署路径
+# $3 部署路径
+# $4 Git分支
 
-if [ -z "$1" ]||[ -z "$4" ]; then
+if [ -z "$1" ]||[ -z "$3" ]; then
     echo "参数不能为空"
     exit 0
 fi
 
 # 创建目录
-mkdir -p $4/$1
-cd $4/$1
+mkdir -p $3/$1
+cd $3/$1
+
+if [ -d "$3/$1/src" ];then
+    cd src
+    git pull
+else
+    git clone $2 src
+    cd src
+fi
 
 # 克隆
-git clone $2 .
-if [ "$3" != "null" ]; then
-    git checkout $3
+if [ "$4" != "null" ]; then
+    git checkout $4
 fi
 
 mvn clean package -Dmaven.test.skip=true
+
+cd ..
+if [ ! -d "$3/$1/pkg" ];then
+    mkdir pkg
+fi
+
+mv src/target/*.jar pkg
